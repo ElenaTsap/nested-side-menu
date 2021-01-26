df05b4247f114b045a093f26badfca48b3b3e11d
ElenaTsap <elenatsapaki@gmail.com>
2021-01-26 10:23:55 +0100

    click on menu-container fixed

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function expand(e) {
-   /*  e.stopPropagation(); */
+    if (e.target.id == 'menu-container') {
+        return e;
+    }
+
     console.log(e.target +`expand`, "i am expanding");
     for (let i = 0; i < e.target.children.length; i++) {
         e.target.children[i].classList.toggle("collapsed");
